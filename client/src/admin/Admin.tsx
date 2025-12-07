7646fccf16f77eb2d492ef9b2fb528a2ea53db5b
Aadil Nuhu <aadilnuhu122@gmail.com>
2025-12-07 22:41:15 +0000

    fix(admin): improve loading indicator and remove debug log

--- client/src/admin/Admin.tsx
+++ client/src/admin/Admin.tsx
@@ -29,7 +29,6 @@ export default function Admin() {
         });
         setLoading(false);
         setUsers(res.data);
-        // console.log(res.data);
       } catch (error: unknown) {
         setError("Failed to fetch users.");
         console.log(error);
@@ -45,7 +44,7 @@ export default function Admin() {
         Waitlist Users
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {loading && <p>Loading users...</p>}
+        {loading && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="#FF17C9" stroke="#FF17C9" strokeWidth="15" r="15" cx="40" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle><circle fill="#FF17C9" stroke="#FF17C9" strokeWidth="15" r="15" cx="100" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle><circle fill="#FF17C9" stroke="#FF17C9" strokeWidth="15" r="15" cx="160" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></circle></svg>}
         {!loading && users.length === 0 && <p>No users found.</p>}
         {users.map((user) => (
           <div
